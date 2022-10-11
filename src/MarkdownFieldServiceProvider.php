3042ccb774f1536baba05f1f70d483aa8949c6e2
Freek Van der Herten <freek@spatie.be>
2022-10-11 13:35:21 +0200

    wip

--- src/MarkdownFieldServiceProvider.php
+++ src/MarkdownFieldServiceProvider.php
@@ -10,16 +10,9 @@ class MarkdownFieldServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('filament-markdown-field')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_filament-markdown-field_table')
-            ->hasCommand(FilamentMarkdownFieldCommand::class);
+            ->hasViews();
     }
 }
