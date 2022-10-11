302d62fec7a3352d9f263f3a76c105fa7712cc39
Freek Van der Herten <freek@spatie.be>
2022-10-11 16:14:37 +0200

    wip

--- src/MarkdownField.php
+++ src/MarkdownField.php
@@ -2,9 +2,14 @@
 
 namespace Spatie\FilamentMarkdownField;
 
+use Filament\Forms\Components\Contracts;
+use Filament\Forms\Components\Concerns;
+
 use Filament\Forms\Components\Field;
 
-class MarkdownField extends Field
+class MarkdownField extends Field implements Contracts\HasFileAttachments
 {
+    use Concerns\HasFileAttachments;
+
     protected string $view = 'filament-markdown-field::markdownField';
 }
