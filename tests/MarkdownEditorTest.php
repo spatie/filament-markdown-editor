0ee08da8d81724bacd0f2e9d604c2e51b24a9676
freekmurze <freekmurze@users.noreply.github.com>
2022-10-13 09:20:46 +0000

    Fix styling

--- tests/MarkdownEditorTest.php
+++ tests/MarkdownEditorTest.php
@@ -7,7 +7,7 @@ use Spatie\FilamentMarkdownEditor\Tests\TestSupport\Livewire;
 it('can render the component', function () {
     $container = ComponentContainer::make(Livewire::make());
 
-    $renderedEditor = (string)MarkdownEditor::make('my-editor')->container($container)->render();
+    $renderedEditor = (string) MarkdownEditor::make('my-editor')->container($container)->render();
 
     expect($renderedEditor)->toBeString();
 });
