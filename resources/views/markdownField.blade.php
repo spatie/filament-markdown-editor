769a20cf3dc026e76beb3b5f8d6312b033ad0b07
Rias <hey@rias.be>
2023-03-17 13:42:25 +0100

    CMD+K should select http placeholder instead of text

--- resources/views/markdownField.blade.php
+++ resources/views/markdownField.blade.php
@@ -102,6 +102,41 @@
                     });
                 },
             });
+            
+            // "Create Link (Ctrl-K)": highlight URL instead of label:
+            editor.codemirror.on('changes', (instance, changes) => {
+                try {
+                    // Grab the last change from the buffered list. I assume the
+                    // buffered one ('changes', instead of 'change') is more efficient,
+                    // and that "Create Link" will always end up last in the list.
+                    const lastChange = changes[changes.length - 1];
+                    if (lastChange.origin === '+input') {
+                        // https://github.com/Ionaru/easy-markdown-editor/blob/8fa54c496f98621d5f45f57577ce630bee8c41ee/src/js/easymde.js#L765
+                        const EASYMDE_URL_PLACEHOLDER = '(https://)';
+                        // The URL placeholder is always placed last, so just look at the
+                        // last text in the array to also cover the multi-line case:
+                        const urlLineText = lastChange.text[lastChange.text.length - 1];
+                        if (urlLineText.endsWith(EASYMDE_URL_PLACEHOLDER) && urlLineText !== '[]' + EASYMDE_URL_PLACEHOLDER) {
+                            const from = lastChange.from;
+                            const to = lastChange.to;
+                            const isSelectionMultiline = lastChange.text.length > 1;
+                            const baseIndex = isSelectionMultiline ? 0 : from.ch;
+                            // Everything works fine for the [Ctrl-K] case, but for the
+                            // [Button] case, this handler happens before the original
+                            // code, thus our change got wiped out.
+                            // Add a small delay to handle that case.
+                            setTimeout(() => {
+                                instance.setSelection(
+                                    { line: to.line, ch: baseIndex + urlLineText.lastIndexOf('(') + 1 },
+                                    { line: to.line, ch: baseIndex + urlLineText.lastIndexOf(')') }
+                                );
+                            }, 25);
+                        }
+                    }
+                } catch (err) {
+                    // Do nothing (revert to original behavior)
+                }
+            });
 
             editor.codemirror.on('change', debounce(() => {
                 state = editor.value();
